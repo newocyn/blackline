70e9d2794544d995eae25191fe35588214c29ea7
newocyn <97858285+newocyn@users.noreply.github.com>
2026-08-13 10:12:56 -0400

    Set Recall Android defaults

--- recall/app/build.gradle.kts
+++ recall/app/build.gradle.kts
@@ -5,4 +5,12 @@ plugins {
 android {
     namespace = "online.pcguys.recall"
     compileSdk = 35
+
+    defaultConfig {
+        applicationId = "online.pcguys.recall"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
 }
