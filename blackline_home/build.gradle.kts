a661dd81e1de597671d31c2eb66361daaa18678b
newocyn <97858285+newocyn@users.noreply.github.com>
2026-08-24 08:01:24 -0400

    Bump BLACKLINE Home to v0.3.1

--- blackline_home/build.gradle.kts
+++ blackline_home/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "online.pcguys.blackline"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "0.3.0"
+        versionCode = 31
+        versionName = "0.3.1"
     }
 
     compileOptions {
