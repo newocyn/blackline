cc16c9d92aeb1b1ea05bfe1fea19040d2aeb7cbe
newocyn <97858285+newocyn@users.noreply.github.com>
2026-08-25 08:06:19 -0400

    Bump BLACKLINE Home to v0.8.1

--- blackline_home/build.gradle.kts
+++ blackline_home/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "online.pcguys.blackline"
         minSdk = 26
         targetSdk = 35
-        versionCode = 80
-        versionName = "0.8.0"
+        versionCode = 81
+        versionName = "0.8.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
